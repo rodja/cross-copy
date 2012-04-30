e1897d9620ec3df1009b40a6e476836446cd04fd
Rodja Trappe <mail@rodja.net>
2012-04-30 08:18:05 +0200

    experimenting with data cache

--- cross-copy.js
+++ cross-copy.js
@@ -1,12 +1,33 @@
 var port = 8124;
 var host = "127.0.0.1";
 
+var url = require('url');
+
+var cache = {};
+
 var http = require('http');
 http.createServer(function (req, res) {
-  if (true) {
-     res.writeHead(200, {'Content-Type': 'text/plain'});
-     res.end('Hello World\n');
+
+  var secret = url.parse(req.url).pathname.substring(1);
+
+  if (req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(secret + '\n');
+  } else if (req.method === 'PUT') {
+      if (cache[secret] != undefined){
+        res.writeHead(403, {'Content-Type': 'text/plain'});
+        res.end('ALREADY IN USE\n');
+      }
+
+      req.on('data', function (chunk) {
+        cache[secret] = chunk;
+         
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('ACCEPTED\n');
+     });
   }
+
+
 }).listen(port, host);
 
 console.log('cross-copy is running at http://' + host + ':' + port + '/');
