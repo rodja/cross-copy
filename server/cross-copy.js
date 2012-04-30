7cc8bf1b3c41c0e6586e0203a56241e92453dd45
Rodja Trappe <mail@rodja.net>
2012-04-30 11:22:37 +0200

    serving web-client with node.js for easy testing

--- server/cross-copy.js
+++ server/cross-copy.js
@@ -17,10 +17,25 @@ http.createServer(function (req, res) {
 
   if (req.method === 'GET') {
     
+    if (secret === ""){
+      fs.readFile('../web-client/index.html', function(error, content) {
+        if (error) {
+            console.log(error);
+            res.writeHead(500);
+            res.end();
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+      });
+      return;
+    }
+
     if (getters[secret] === undefined) getters[secret] = [];
     getters[secret].push(res);
  
- } else if (req.method === 'PUT') {
+  } else if (req.method === 'PUT') {
 
       if (getters[secret] == undefined){
         res.writeHead(404, header);
@@ -44,3 +59,50 @@ http.createServer(function (req, res) {
 }).listen(port, host);
 
 console.log('cross-copy is running at http://' + host + ':' + port + '/');
+
+
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+ 
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+    filePath = '../web-client/' + filePath;    
+  
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
+}).listen(++port);
+
+console.log('web-client is running at http://' + host + ':' + port + '/');
+
