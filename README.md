92efe2f9979665d190f3eab4e1795673810152db
Rodja Trappe <mail@rodja.net>
2012-05-02 05:58:25 +0200

    added license info

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # cross copy
 
 The web app http://cross-copy.net attacs the problem when you need to copy and paste between different devices. The backend is written in node.js. There is also a commandline tool for sending and receiving texts when working on a server.
+
+## Use Cases
+
+## License
+
+Copyright (c) Rodja Trappe
+Licensing: GPL v3
+
+See COPYING.txt for the GNU GENERAL PUBLIC LICENSE
--- server/cross-copy.js
+++ server/cross-copy.js
@@ -1,8 +1,28 @@
+//
 // cross-copy.net server implemented in node.js 
 //
 // usage:
 //    GET /api/<your secret code>      wait's for data on the given phrase
 //    PUT /api/<your secret code>      sends data in body to all waiting clients
+
+/*  
+    Copyright 2012 Rodja Trappe
+
+    This file is part of cross copy.
+
+    cross copy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cross copy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cross copy.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var port = 8080;
 
