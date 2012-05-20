00d1ecb6d949fe3cb21e72ff5a299cfcdfecd1e9
Rodja Trappe <mail@rodja.net>
2012-05-20 07:00:08 +0200

    added 'no deliveries' test

--- server/function-tests.sh
+++ server/function-tests.sh
@@ -3,15 +3,24 @@
 TEST="../cmd-client/cross-copy -q -l"
 
 report(){
-  echo $@
+  
+  echo "$@ in line" `caller 1`
 }
 
 assertEqual(){
-  [[ $1 == $2 ]] || report "$3 (expected '$2' but was '$1')"
+  [[ $1 == $2 ]] || report "$3 (expected '$1' but was '$2')"
 }
+
+##### FUNCTION TESTS
+
+R=`$TEST some-secret "$DATA"`
+assertEqual 0 $R "shoud have no direct deliverys"
 
 DATA="the message"
 ( M=`$TEST some-secret`; assertEqual "$DATA" "$M" "should receive correct message" ) &
 sleep 1
 R=`$TEST some-secret "$DATA"`
 assertEqual 1 $R "shoud have one direct delivery"
+
+
+
