6035188c711b8be8a8855533ef6580ee44d3b9bf
Amelisoft <bures@amelisoft.com>
2012-06-07 22:21:31 +0200

    Restructure iOS App User Interface
    - master/detail layout
    - saving history (serializing to NSUserDefaults)

--- ios-client/crosscopy-ios-client/BusinessObjects.cs
+++ ios-client/crosscopy-ios-client/BusinessObjects.cs
@@ -1,25 +1,49 @@
 using System;
+using System.Collections.Generic;
+using MonoTouch.Foundation;
+using System.Xml.Serialization;
+using System.ComponentModel;
 
 namespace CrossCopy.iOSClient.BL
 {
-    public enum DataItemDirection
+	[System.Diagnostics.DebuggerDisplay("History - Secrets {Secrets.Count}")]
+    [XmlRoot("history")]
+    public class History
     {
-        In,
-        Out
+        public History()
+        {
+            Secrets = new List<Secret>();
+        }
+
+        [XmlElement("secret")]
+        public List<Secret> Secrets { get; set; }
     }
-    
+
+    [System.Diagnostics.DebuggerDisplay("Secret - {Phrase} {DataItems.Count}")]
+    public class Secret
+    {
+        public Secret() 
+        {
+            DataItems = new List<DataItem>();
+        }
+        
+        public Secret(string phrase) 
+        {
+            Phrase =  phrase;
+            DataItems = new List<DataItem>();
+        }
+
+        [XmlAttribute("phrase")]
+        public string Phrase { get; set; }
+        [XmlElement("dataitem")]
+        public List<DataItem> DataItems { get; set; }
+
+    }
+
+    [System.Diagnostics.DebuggerDisplay("DataItem - {Data}")]
     public class DataItem
     {
-        #region Properties
-        public string Data { get; set; }
-
-        public DataItemDirection Direction { get; set; }
-
-        public DateTime Date { get; set; }
-        #endregion
-        
-        #region Ctor
-        public DataItem ()
+        public DataItem()
         {
         }
         
@@ -29,7 +53,28 @@ namespace CrossCopy.iOSClient.BL
             Direction = direction;
             Date = date;
         }
-        #endregion
+
+        [XmlAttribute("data")]
+        public string Data { get; set; }
+
+        [XmlIgnore]
+        public DataItemDirection Direction { get; set; }
+        [XmlAttribute("direction")]
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        public int DirectionInt 
+        {
+            get { return (int)Direction; }
+            set { Direction = (DataItemDirection)value; }
+        }
+
+        [XmlAttribute("date")]
+        public DateTime Date { get; set; }
+    }
+    
+    public enum DataItemDirection
+    {
+        In,
+        Out
     }
 }
 
