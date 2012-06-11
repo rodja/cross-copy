5a644a202c21b0c9f4b2359c6c062c07294bb872
Rodja Trappe <mail@rodja.net>
2012-06-11 05:35:16 +0200

    removed obsolete code

--- ios-client/crosscopy-ios-client/AppDelegate.cs
+++ ios-client/crosscopy-ios-client/AppDelegate.cs
@@ -365,52 +365,7 @@ namespace CrossCopy.iOSClient
             webClient.DownloadDataCompleted += new DownloadDataCompletedEventHandler (dwnldCompletedDelegate);
             webClient.DownloadDataAsync (url);
         }
-        
-        public static int DownloadFile (string remoteFilename, string localFilename)
-        {
-            int bytesProcessed = 0;
-         
-            Stream remoteStream = null;
-            Stream localStream = null;
-            WebResponse response = null;
-            
-            try {
-                WebRequest request = WebRequest.Create (remoteFilename);
-                if (request != null) {
-                    response = request.GetResponse ();
-                    if (response != null) {
-                        remoteStream = response.GetResponseStream ();
-            
-                        localStream = File.Create (localFilename);
-            
-                        byte[] buffer = new byte[1024];
-                        int bytesRead;
-            
-                        do {
-                            bytesRead = remoteStream.Read (
-                                buffer,
-                                0,
-                                buffer.Length
-                            );
-                            localStream.Write (buffer, 0, bytesRead);
-                            bytesProcessed += bytesRead;
-                        } while (bytesRead > 0);
-                    }
-                }
-            } catch (Exception e) {
-                Console.Out.WriteLine (e.Message);
-            } finally {
-                if (response != null)
-                    response.Close ();
-                if (remoteStream != null)
-                    remoteStream.Close ();
-                if (localStream != null)
-                    localStream.Close ();
-            }
-            
-            return bytesProcessed;
-        }
-        
+
         private void OpenFile (string fileName)
         {
             var sbounds = UIScreen.MainScreen.Bounds;
